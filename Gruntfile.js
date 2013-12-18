95ff43b898ee603ae6bef40b1b40892fc796824f
David Mohl <david.mohl@ymail.com>
2013-12-19 01:52:22 +0900

    added building to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,48 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-    karma: {
-        unit: {
-            configFile: 'karma.conf.js'
+module.exports = function (grunt) {
+    grunt.initConfig({
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
+        uglify: {
+            compress: {
+                options: {
+                    mangle: true,
+                    compress: true
+                },
+                files: {
+                    'dist/facebook-ng.min.js': ['src/js/facebook-ng.js']
+                }
+            },
+            beautify: {
+                options: {
+                    beautify: true
+                },
+                files: {
+                    'dist/facebook-ng.js': ['src/js/facebook-ng.js']
+                }
+            }
+        },
+        coffee: {
+            compile: {
+                files: {
+                    'src/js/facebook-ng.js': 'src/coffee/facebook-ng.coffee',
+                    'test/js/facebookInitSpec.js': 'test/coffee/facebookInitSpec.coffee',
+                    'test/js/tagGenerationSpec.js': 'test/coffee/tagGenerationSpec.coffee'
+                }
+            }
         }
-    }
-  });
+    });
 
-  // Register tasks.
-  grunt.loadNpmTasks('grunt-karma');
+    // Register tasks.
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
 
-  // Default task.
-  grunt.registerTask('test', 'karma');
-  grunt.registerTask('default', 'test');
+
+    grunt.registerTask('test', 'karma');
+    grunt.registerTask('build', ['coffee', 'uglify:compress', 'uglify:beautify'])
+
+    grunt.registerTask('default', 'test');
 };
